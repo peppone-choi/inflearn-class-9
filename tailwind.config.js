cae74c7e8188a94a449e81acf37609dd124eabd9
peppone-choi <peppone.choi@gmail.com>
2024-05-07 20:33:47 +0900

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
-  darkMode: false, // or 'media' or 'class'
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
